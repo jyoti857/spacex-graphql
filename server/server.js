6f5a42d299214bbf94a55cd3049c5115bbeab72f
Jyoti RS <jyotiranjans@cnxsi.com>
2019-12-17 17:53:10 +0530

    server initial sesetup done

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const app = express();
+const schema = require('./schema')
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true 
+}));
+
+const port  = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`server started listening on ${port}`));
